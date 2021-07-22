c583cedb6a7c2576f371148399ec6f9cc4c5e312
Stephen Foster <stephenfostercpp@gmail.com>
2021-07-21 18:20:10 -0700

    draft

--- src/pchgroups.lua
+++ src/pchgroups.lua
@@ -99,90 +99,99 @@ premake.override(premake.vstudio.vc2010.elements, "clCompile", function(base, cf
 
 end)
 
-function pchgroup(...)
-	--default src dir: wks.location\src\
-	--default int dir: wks.location\build\obj\plat_config
-	local arg={...}
-	local n = 0
+function isPath(str)
+	if str and #str > 0 then
+		for i = 1, #str do
+			if str[i] == '/' or str[i] == '\\' then
+				return true 
+			end
+		end
+	end
+	return false
+end
 
-	local pchname = "sharedpch"
-	local pchfilename = "stdafx"
+function pchgroup(prjgroup, ...)
 
-	local ms_pchsrcdir = "$(SolutionDir)\\src\\$(ProjectName)\\"
-	local ms_pchtargetdir = "$(SolutionDir)\\build\\bin\\$(ProjectName)\\$(Configuration)-$(PlatformShortName)\\"
-	local ms_pchobjdir = "$(SolutionDir)\\build\\obj\\$(ProjectName)\\$(Configuration)-$(PlatformShortName)\\"
+	slndir = path.translate(_MAIN_SCRIPT_DIR)
+	keydir = "$(Configuration)-$(Platform)\\"
 
+	pchprjname   = "sharedpch"
+	pchfilename  = "stdafx.h"
+	pchsrcdir    = (slndir.. "\\src\\sharedpch\\") --%{prj.name}\\")
+	pchtargetdir = (slndir.. "\\build\\bin\\$(ProjectName)\\" .. keydir)
+	pchobjdir    = (slndir.. "\\build\\obj\\$(ProjectName)\\" .. keydir)
 
-	if #arg > 2 then
+	arg={...}
+	--arg[2]
+	--arg[3]
+	--arg[4]
+	--arg[5]
+	--arg[6]
 
-		n = 2
-
-		pchname = arg[1]
-
+	
+	if #arg > 2 and not isPath(arg[1]) then
+		pchprjname = arg[1]
 	end
 
-	if #arg > 3 then
-
-		n = 3
-
-		pchsrcdir = arg[2]
-
+	if #arg > 3 and isPath(arg[2]) then
+		pchsrcdir = (slndir .. "\\" .. path.translate(arg[2]))
 	end
 
-	if #arg > 4 then
-
-		n = 4
-
-		pchobjdir = arg[3]
-
+	if #arg > 4 and isPath(arg[3]) then
+		pchobjdir = (slndir .. "\\" .. path.translate(arg[3]))
 	end
 
-	--create staticlib
-	project (pchname)
-		kind "StaticLib"
-		location (ms_pchsrcdir)
-		targetdir(ms_pchtargetdir)
-		objdir(ms_pchobjdir)
+	project (pchprjname)
+		kind      "StaticLib"
+		location  (pchsrcdir)
+		targetdir (pchtargetdir)
+		objdir    (pchobjdir)
 
 		files
 		{
-			(ms_pchsrcdir .. "/**.h"),
-			(ms_pchsrcdir .. "/**.hpp"),
-			(ms_pchsrcdir .. "/**.cpp")
+			(pchsrcdir .. "/**.h"),
+			(pchsrcdir .. "/**.hpp"),
+			(pchsrcdir .. "/**.cpp")
 		}
 
-		pchheader (pchfilename .. ".h")
+		pchheader (pchfilename)
 		pchsource ("$(SharedPch)")
+	project "*"
 
-	prjlist = {}
-	index = 1
+	prjgroup = {}
 
-	for i = n, #arg do
-		prjlist[index] = arg[i]
-		index = index + 1
+	for i = 1, #arg do
+		local val = arg[#arg + 1 - i]
+
+		if not isPath(val) and val ~= pchprjname then
+			prjgroup[i] = val
+		
+		else
+			break
+		end
 	end
 
-	table.insert(prjlist, pchname)
+	--reserve adding the pchprj to do so explicitly as follows:
+	table.insert(prjgroup, pchprjname)
 
-	for i = 1, #prjlist do
-		print(prjlist[i])
+	for i = 1, #prjgroup do
+		print(prjgroup[i])
 	end
 
 	--apply compat props to projects down a list
 	--do so by creating a list of projects and then add the pchname to the list
 
 
-	for i = 1, #prjlist do
-		project (prjlist[i])
+	for i = 1, #prjgroup do
+		project (prjgroup[i])
 			---[[
 			usermacros
 			{
-				SharedPch=(ms_pchtargetdir),
-				SharedPdb=(ms_pchobjdir),
-				SharedIdb=(ms_pchsrcdir)
+				SharedPch=(pchtargetdir),
+				SharedPdb=(pchobjdir),
+				SharedIdb=(pchsrcdir)
 			}
 			--]]
-
 		project "*"
 		--usermacros
 		--pdbfile
@@ -197,4 +206,8 @@ function pchgroup(...)
 
 
 	end
+
+	ok, err = os.touchfile(pchsrcdir .. pchfilename)
+
+	print(ok, err)
 end
